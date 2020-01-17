1cc04c259859f37479dd8683d4dcd11e7f75385e
dpoemmer <d.poemmer@htlkrems.at>
2020-01-17 10:02:16 +0100

    player death fix and swinging ball

--- Prj/Levels/Level_Base.gd
+++ Prj/Levels/Level_Base.gd
@@ -1,15 +1,11 @@
 extends Node2D
 
-#var p = preload("res://Player/Player.tscn")
 var player
 var last_checkpoint
 
 func _ready():
-	#player = p.instance()
-	#add_child(player)
+	player = $Player
 	last_checkpoint = $Start
-	
-	#player.position = last_checkpoint.position
 
 func new_checkpoint(checkpoint):
 	last_checkpoint = checkpoint
