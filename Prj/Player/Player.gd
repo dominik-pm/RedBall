941114a456ed66d276d334132b2c4363f7c04d59
dpoemmer <d.poemmer@htlkrems.at>
2020-01-17 09:51:37 +0100

    player movement

--- Prj/Player/Player.gd
+++ Prj/Player/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var death_particles = preload("res://Player/Death_Particles.tscn")
 
 export var jump_force = 450
+export var jump_hold_boost = 200
+export var jump_hold_boost_duration = 100
+export var jump_bounce_boost = 50
 export var movement_speed = 11
 export var max_speed = 390
 export var gravity = 11
@@ -12,6 +15,9 @@ export var damp = 0.99
 
 var velocity = Vector2(0,0)
 var dir = 0
+var jumpcount = 0
+var jump_hold = false
+var can_bounce = true
 
 onready var level = $"../"
 
@@ -24,8 +30,24 @@ func _physics_process(delta):
 		dir -= movement_speed
 	if Input.is_action_pressed("move_right"):
 		dir += movement_speed
+	
+	if jump_hold and Input.is_action_pressed("jump") and !is_on_floor() and jumpcount < jump_hold_boost_duration:
+		jumpcount += 1
+		velocity.y -= jump_hold_boost*delta
+	else:
+		jump_hold = false
+	
 	if Input.is_action_pressed("jump") and is_on_floor():
+		if can_bounce:
+			velocity.y -= jump_bounce_boost
 		velocity.y -= jump_force
+		jumpcount = 0
+		jump_hold = true
+	
+	if Input.is_action_pressed("jump"):
+		can_bounce = true
+	else:
+		can_bounce = false
 	
 	dir = clamp(dir, -max_speed, max_speed)
 	dir = dir*damp
